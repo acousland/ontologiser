a7e3c299bf62f8131a02d6398f5a1719e2632f51
Aaron Cousland <acousland@gmail.com>
2023-10-02 13:33:55 +1100

    Initial commit

---
+++ ontologiser.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+
+    option = st.selectbox("Which column has your data in it?",list(dataframe.columns),
+#                 index=None,
+                 placeholder="Select contact method...",)
+    st.write('You selected:', option)
+
+    st.slider('How many groups are you looking for?', 0, 25, 8)
+    title = st.text_input('Any additional context for the AI to consider', '')
+    if st.button("Generate domains", type="secondary"):
+        st.write('Potatoes; Donkeys')
+
+        st.graphviz_chart('''
+        graph FAQ {
+    node [shape=rectangle, style=filled, color=lightyellow]
+
+    a [label="IM Health Check"]
+    b [label="Duration"]
+    c [label="Deliverables"]
+    d [label="Benchmark Model"]
+    e [label="Data Insights"]
+    f [label="IM Systems Approach"]
+    g [label="Architectural Framework"]
+    h [label="Data Management"]
+    i [label="Understanding Data"]
+    j [label="Data Quality"]
+    k [label="Business Benefits"]
+    l [label="Compliance & Standards"]
+
+    // Create edges to show the relations between different nodes
+    a -- b
+    a -- c
+    a -- d
+    a -- f
+    b -- c
+    c -- f
+    d -- l
+    e -- i
+    e -- k
+    f -- g
+    f -- h
+    f -- k
+    g -- h
+    h -- i
+    h -- j
+    i -- j
+    j -- k
+    k -- l
+}
+    }
+''')
+
